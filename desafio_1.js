408267bd5efbc9b99e44a42367323f0ee7483999
DavidWesleyS <davidwesleys2003@gmail.com>
2023-11-25 20:07:16 -0300

    Atualização
    Atualização final

---
+++ desafio_1.js
@@ -0,0 +1,26 @@
+//Desafio em JavaScript
+
+    let numeroOito = 8
+    let stringOito = '8'
+    let numeroVinte = 20
+    let stringVinte = '20'
+    let numeroSeis = 6
+    let stringSeis = '6'
+    
+    if ( numeroOito == stringOito) {
+        console.log('As variaveis numeroOito e stringOito tem o mesmo valor, mas tipos diferentes')
+    } else {
+        console.log('As variáveis numeroOito e stringOito não tem o mesmo valor')
+    }
+
+    if (numeroVinte === stringVinte) {
+        console.log('As variáveis numeroVinte e stringVinte tem o mesmo valor e mesmo tipo')
+    } else{
+        console.log('as variáveis numeroVinte e stringVinte não tem o mesmo tipo')
+    }
+
+    if ( numeroSeis == stringSeis) {
+        console.log('As variáveis numeroSeis e stringSeis tem o mesmo valor, mas tipos diferentes')
+    } else {
+        console.log('As variáveis mumeroSeis e stringSeis não tem o mesmo valor')
+    }
